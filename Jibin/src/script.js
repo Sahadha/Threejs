6424889283e2ae557b52c0a588f77f6e18a524a6
1622710tcs <1622710tcs@01hw1764996.bbrouter>
2022-01-16 13:19:10 +0530

    Window Resizing for threejs

--- Jibin/src/script.js
+++ Jibin/src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import *as THREE from 'three'
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 const canvas = document.querySelector('.webgl')
 //creating basic scene
 const scene = new THREE.Scene()
@@ -11,9 +11,22 @@ scene.add(mesh)
 
 //defining canvas size
 const sizes ={
-    width:800,
-    height:600
+    width:window.innerWidth,
+    height:window.innerHeight
 }
+
+//window resizing problem fix
+
+window.addEventListener('resize',()=>{
+    //update sizes
+    sizes.width=window.innerWidth,
+    sizes.height=window.innerHeight
+
+    camera.aspect = sizes.width/sizes.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width,sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+})
 
 //creating camera
 const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height)
@@ -21,10 +34,23 @@ camera.position.x=0
 camera.position.y=0
 camera.position.z=5
 scene.add(camera)
-
+const controls = new OrbitControls(camera,canvas)
+controls.enableDamping=true
 //render scene and camera
 const renderer = new THREE.WebGLRenderer({
     canvas:canvas,
 
 })
-renderer.render(scene,camera)
+renderer.setSize(sizes.width,sizes.height)
+
+//updating frames for animation
+const clock = new THREE.Clock()
+const tick = ()=>
+{
+    const elapsedTime = clock.getElapsedTime()
+    controls.update()
+    renderer.render(scene,camera)
+    window.requestAnimationFrame(tick)
+
+}
+tick()
